b66aa705bbb2e118f69c27559a9af31d45d607eb
Maciej Dudkowski <52274813+dudurol6@users.noreply.github.com>
2022-12-18 10:54:20 +0000

    Added icons

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"> 
     <meta name="apple-mobile-web-app-capable" content="yes"></meta>
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
+    <meta name="apple-mobile-web-app-status-bar-style" content="default"></meta>
     <meta name="author" content="Maciej Dudkowski">
     <title><?php echo title; ?></title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -15,6 +15,15 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;700&display=swap" rel="stylesheet">
+	<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
+	<link rel="manifest" href="/icons/site.webmanifest">
+	<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="shortcut icon" href="/icons/favicon.ico">
+	<meta name="msapplication-TileColor" content="#a05936">
+	<meta name="msapplication-config" content="/icons/browserconfig.xml">
+	<meta name="theme-color" content="#a05936">
 
 
     <link rel="stylesheet" href="../fontello/css/fontello.css">
@@ -31,8 +40,8 @@
                 <a href="strona-glowna.php">
                     <div class="header-nav-item">Strona Główna</div>
                 </a>
-                <a href="#">
-                    <div class="header-nav-item" style="color: #a3a3a3!important;cursor:default;"><strike>Posty</strike></div>
+                <a href="posty.php">
+                    <div class="header-nav-item">Posty</div>
                 </a>
                 <a href="o-autorze.php">
                     <div class="header-nav-item">O autorze</div>
---
+++ main_files/posty.php
@@ -0,0 +1,21 @@
+<?php
+
+    define('title', 'PG - Posty');
+    include('../includes/header.php');
+
+?>
+
+<main>
+
+    <div class="main-welcome-text">Posty</div>
+    <div class="main-box">
+
+		<div class="main-elem">
+			
+		</div>
+
+    </div>
+
+</main>
+
+<?php include('../includes/footer.php'); ?>
